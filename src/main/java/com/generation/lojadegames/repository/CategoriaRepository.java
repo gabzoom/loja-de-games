711b06bff42f70cc1cdb7557b245625c10e0893c
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2023-12-14 22:14:49 -0300

    :sparkles: feat: Adiciona a Interface de Categoria

---
+++ src/main/java/com/generation/lojadegames/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.lojadegames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.lojadegames.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
