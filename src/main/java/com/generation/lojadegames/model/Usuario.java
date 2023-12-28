23bf0ba8b453f9cf0b45bd729753e93228521cc0
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2023-12-28 18:38:12 -0300

    :sparkles: feat: Adiciona as classes model Usuario e UsuarioLogin

---
+++ src/main/java/com/generation/lojadegames/model/Usuario.java
@@ -0,0 +1,94 @@
+package com.generation.lojadegames.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo Nome é obrigatório!")
+	private String nome;
+
+	@NotBlank(message = "O atributo Usuário é obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+	private String usuario;
+
+	@NotBlank(message = "O atributo Senha é obrigatório!")
+	@Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres.")
+	private String senha;
+
+	@Column(length = 5000)
+	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres.")
+	private String foto;
+
+	@NotNull(message = "O atributo Data de Nascimento é obrigatório!")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataNascimento;
+
+	// Getters e Setters
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return this.foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
